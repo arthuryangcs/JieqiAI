05918101a6f8c171bf4c3ef6d81aa1bcc99d75e0
yangyemeng <yangyemeng@bytedance.com>
2021-09-22 13:36:32 +0800

    makefile

---
+++ frontend/Makefile
@@ -0,0 +1,2 @@
+build:
+	npm run make
--- frontend/index.js
+++ frontend/index.js
@@ -1,17 +1,18 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
+
 
   win.loadFile('index.html')
 }
-
-app.whenReady().then(() => {
-  createWindow()
-})
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
@@ -24,4 +25,3 @@ app.whenReady().then(() => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
-
